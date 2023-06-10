48da02eadee7586b25dd564a914336c67415c17f
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-06-10 06:36:15 +0530

    add component to fetch data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 /* eslint-disable linebreak-style */
 import './App.css';
 import Calculator from './component/calculator';
+import FetchData from './component/fetchdata';
 
 function App() {
   return (
     <>
       <div className="App">
         <Calculator />
+      </div>
+      <div className="quote">
+        <FetchData />
       </div>
     </>
   );
